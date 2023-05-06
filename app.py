28f16ef869fc108271490b5e6dd0a1cedb71ae79
Gigi <gigi_cheang@hotmail.com>
2023-05-05 22:21:20 -0400

    Add summarize command

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ import os
 import discord
 from dotenv import load_dotenv
 
+import cohere
+
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
+API_KEY = os.getenv('API_KEY')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+
+co = cohere.Client(API_KEY)
 
 @client.event
 async def on_ready():
@@ -22,5 +27,24 @@ async def on_message(message):
 
     if message.content.startswith('!ping'):
         await message.channel.send('Pong!')
+        return
+    
+    if message.content.startswith('!cohere'):
+        response = await generate(" ".join(message.content.split()[1:]))
+        await message.channel.send(response)
+        return
+        
+async def generate(message):
+    print("message:", message)
+    response = co.summarize( 
+        text=message,
+        length='auto',
+        format='auto',
+        model='summarize-xlarge',
+        additional_command='',
+        temperature=0.3,
+    )
+    print("response:", response.summary)
+    return response.summary
 
 client.run(TOKEN)
