c5b41e5982e072e7143f6506524f51cff801ce08
Gigi <gigi_cheang@hotmail.com>
2023-05-05 21:19:58 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!ping'):
+        await message.channel.send('Pong!')
+
+client.run(TOKEN)
